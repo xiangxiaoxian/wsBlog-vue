c4c931df2824e92a028915b6be42b6d349befc32
xiangxiaoxian <1924837954@qq.com>
2021-02-22 20:40:53 +0800

    登录页面进行修改，以前需要数据太繁琐

--- src/views/login.vue
+++ src/views/login.vue
@@ -18,18 +18,6 @@
               placeholder="请输入手机号/账号"
             ></el-input>
           </el-form-item>
-          <el-form-item prop="smscode" class="code">
-            <el-input
-              v-model="ruleForm2.smscode"
-              placeholder="验证码"
-            ></el-input>
-            <el-button
-              type="primary"
-              :disabled="isDisabled"
-              @click="sendCode"
-              >{{ buttonText }}</el-button
-            >
-          </el-form-item>
           <el-form-item prop="pass">
             <el-input
               type="password"
@@ -38,18 +26,6 @@
               placeholder="输入密码"
             ></el-input>
           </el-form-item>
-          <el-form-item prop="checkPass">
-            <el-input
-              type="password"
-              v-model="ruleForm2.checkPass"
-              auto-complete="off"
-              placeholder="确认密码"
-            ></el-input>
-          </el-form-item>
-
-      
-          
-
           <el-form-item>
             <el-button
               type="primary"
@@ -67,58 +43,10 @@
 export default {
   name: "login",
   data() {
-    // <!--验证手机号是否合法-->
-    let checkTel = (rule, value, callback) => {
-      if (value === "") {
-        callback(new Error("请输入手机号码"));
-      } else if (!this.checkMobile(value)) {
-        callback(new Error("手机号码不合法"));
-      } else {
-        callback();
-      }
-    };
-    //  <!--验证码是否为空-->
-    let checkSmscode = (rule, value, callback) => {
-      if (value === "") {
-        callback(new Error("请输入手机验证码"));
-      } else {
-        callback();
-      }
-    };
-    // <!--验证密码-->
-    let validatePass = (rule, value, callback) => {
-      if (value === "") {
-        callback(new Error("请输入密码"));
-      } else {
-        if (this.ruleForm2.checkPass !== "") {
-          this.$refs.ruleForm2.validateField("checkPass");
-        }
-        callback();
-      }
-    };
-    // <!--二次验证密码-->
-    let validatePass2 = (rule, value, callback) => {
-      if (value === "") {
-        callback(new Error("请再次输入密码"));
-      } else if (value !== this.ruleForm2.pass) {
-        callback(new Error("两次输入密码不一致!"));
-      } else {
-        callback();
-      }
-    };
     return {
       ruleForm2: {
         pass: "",
-        checkPass: "",
         tel: "",
-        smscode: "",
-        email:''
-      },
-      rules2: {
-        pass: [{ validator: validatePass, trigger: "change" }],
-        checkPass: [{ validator: validatePass2, trigger: "change" }],
-        tel: [{ validator: checkTel, trigger: "change" }],
-        smscode: [{ validator: checkSmscode, trigger: "change" }],
       },
       buttonText: "发送验证码",
       isDisabled: false, // 是否禁止点击发送验证码按钮
@@ -129,35 +57,12 @@ export default {
 this.$emit('public_header',false)
   },
   methods: {
-    // <!--发送验证码-->
-    sendCode() {
-      let tel = this.ruleForm2.tel;
-      if (this.checkMobile(tel)) {
-        console.log(tel);
-        let time = 60;
-        this.buttonText = "已发送";
-        this.isDisabled = true;
-        if (this.flag) {
-          this.flag = false;
-          let timer = setInterval(() => {
-            time--;
-            this.buttonText = time + " 秒";
-            if (time === 0) {
-              clearInterval(timer);
-              this.buttonText = "重新获取";
-              this.isDisabled = false;
-              this.flag = true;
-            }
-          }, 1000);
-        }
-      }
-    },
-    // <!--提交注册-->
+    // <!--提交登录-->
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
           setTimeout(() => {
-            alert("注册成功");
+            alert("登录成功");
           }, 400);
         } else {
           console.log("error submit!!");
@@ -170,15 +75,6 @@ this.$emit('public_header',false)
       this.$router.push({
         path: "/login",
       });
-    },
-    // 验证手机号
-    checkMobile(str) {
-      let re = /^1\d{10}$/;
-      if (re.test(str)) {
-        return true;
-      } else {
-        return false;
-      }
     },
   // 烟花背景
 
