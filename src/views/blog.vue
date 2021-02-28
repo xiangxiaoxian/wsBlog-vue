5a71cf3886b5972ec775604fe89108ff6302c6b2
2510998796 <2510998796@qq.com>
2021-02-28 16:29:13 +0800

    blog

--- src/views/blog.vue
+++ src/views/blog.vue
@@ -2,19 +2,19 @@
   <div class="article">
     <div class="article_list">
       <div class="article_listone">
-        标题<el-input></el-input>
+        标题:<el-input class="articleInput" style="width:600px" placeholder="请输入标题："></el-input>
       </div>
       <div class="article_list_center">
-       分类：
-          <el-select v-model="value" placeholder="请选择分类">
-            <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value">
-            </el-option>
-          </el-select>
-
+        分类：
+        <el-select v-model="value" placeholder="请选择分类">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          >
+          </el-option>
+        </el-select>
       </div>
       <div class="article_listtwo">
         标签：
@@ -23,7 +23,8 @@
             v-for="item in options"
             :key="item.value"
             :label="item.label"
-            :value="item.value">
+            :value="item.value"
+          >
           </el-option>
         </el-select>
       </div>
@@ -31,9 +32,7 @@
     <div class="blog_makdown">
       <h3>写博客</h3>
       <mavon-editor v-moudle="value" />
-      <p>
-
-      </p>
+      <p></p>
     </div>
   </div>
 </template>
@@ -43,34 +42,32 @@ export default {
   data() {
     return {
       value: "",
-      lable:{
-        id:"",
-        lableName:"",
-        lableComments:"",
+      lable: {
+        id: "",
+        lableName: "",
+        lableComments: "",
       },
-      sort:{
-        id:"",
-        sortName:"",
-        sortComments:"",
+      sort: {
+        id: "",
+        sortName: "",
+        sortComments: "",
       },
-      article:{
-        id:"",
-        userId:"",
-        title:"",
-        content:"",
+      article: {
+        id: "",
+        userId: "",
+        title: "",
+        content: "",
       },
       defaultData: "preview",
     };
   },
-  mounted() {
-
+  mounted() {},
+  methods: {
+    getLable() {
+      const _this = this;
+      _this.$axios.get("/");
+    },
   },
-  methods:{
-    getLable(){
-      const _this=this;
-      _this.$axios.get("/")
-    }
-  }
 };
 </script>
 
@@ -80,53 +77,54 @@ export default {
   background-color: #fff;
 }
 
-.article_list{
-    margin-top: 6px;
-    width: 70%;
-    /* height: 80px; */
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    background: rgba(255, 255, 255, 2);
+.article_list {
+  /* margin-top: 6px;
+  width: 70%;
+ 
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background: rgba(255, 255, 255, 2); */
 }
 
-.article .article_list:hover{
-    background:#F0F0F5;
-
+.article .article_list:hover {
+  /* background: #f0f0f5; */
 }
-.article_list .article_listone h2:hover{
-  color: orange;
+.article_list .article_listone h2:hover {
+  /* color: orange; */
 }
 
-.article_listone {
-  display: flex;
-  justify-content: space-between;
-  margin: 2px 8px;
-
-  /* border: 1px solid #ccc; */
+.article_listone  {
+  /* display: flex; */
+  margin: 8px 10px;
+  padding: 2px 4px; 
 }
-.article_list_center{
-    margin: 14px 8px;
-    padding: 2px 0;
+.article_listone  .articleInput{
+  /* display: flex; */
+  margin: 8px 10px;
+  /* padding: 2px 4px;  */
+}
+
+.article_list_center {
+   margin: 14px 8px; 
+  padding: 2px 0; 
 }
 .article_listtwo {
-    margin: 14px 8px;
-    padding: 2px 0;
-  display: flex;
-  justify-content: space-between;
+   margin: 14px 8px;
+  padding: 2px 0;
+ 
 }
 .article_listtwo_right ul {
-  display: flex;
-  padding:  0 10px;
-  margin: 0 8px;
+  /* display: flex;
+  padding: 0 10px;
+  margin: 0 8px; */
   /* background: red; */
 }
-.article_listtwo_right ul  li{
-  padding:  0 4px;
-  margin: 0 8px;
-  /* background: red; */
+.article_listtwo_right ul li {
+  /* padding: 0 4px;
+  margin: 0 8px; */
 }
 
 .article_listtwo_center {
-  color: silver;
+  /* color: silver; */
 }
 </style>
